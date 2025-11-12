8c9a98708a93677406140866e382ed3e7ce5f922
Muhamed Shihas <muhamedshihas001.com>
2025-11-12 04:44:57 +0000

    initial jenkins+docker compose project

--- api/app.py
+++ api/app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return "hello from API connected Successfully"
-
+    return "Hello from API! Connected successfully."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
